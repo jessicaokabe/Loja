c5a24ed9ae5171d039ffb2d06c2bf05cdb22fc74
Jessica Okabe <jessicaokabe@hotmail.com>
2022-07-23 00:01:42 -0300

    tela de produtos

---
+++ Loja.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,35 @@
+﻿using Loja.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loja.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new List<ProdutoViewModel>() { 
+                new ProdutoViewModel { Id= 1, Nome = "Banana" }, 
+                new ProdutoViewModel { Id= 2, Nome = "Maca" },
+                new ProdutoViewModel { Id= 3, Nome = "kiwi" },
+            };
+            return View(model);
+        }
+
+        public IActionResult Save(int id)
+        {
+            var model = new ProdutoViewModel() { Id= 1, Nome = "Banana" };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Save(ProdutoViewModel model)
+        {
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Loja.Web/Models/ProdutoViewModel.cs
@@ -0,0 +1,8 @@
+namespace Loja.Web.Models
+{
+    public class ProdutoViewModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+    }
+}
